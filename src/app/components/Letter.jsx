a9a8d8715c470a5a57bda72201c533db0434cd13
Mohammed Abdul Khader <abdulkhader0404@gmail.com>
2025-09-30 19:22:56 +0300

    Update Letter.jsx

--- src/app/components/Letter.jsx
+++ src/app/components/Letter.jsx
@@ -22,7 +22,7 @@ Thank you for being the wonderful, amazing, absolutely fantastic person that you
 Happy Birthday, Mahira Kaleem, you beautiful soul! 🎂✨
 
 With all my love and warmest wishes,
-Abbu :) :( 🌸 💕`
+Abbu 😊 😞 🌸 💕`
 
     useEffect(() => {
         if (showText) {
